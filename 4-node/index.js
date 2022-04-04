c0a7bcf74933edb01a9e7bde884d6b68cc3f1d14
Amilcar J. Ruiz <98772743+AmilcarJRuiz@users.noreply.github.com>
2022-04-04 14:40:50 +0200

    correciones

--- 4-node/index.js
+++ 4-node/index.js
@@ -1,17 +1,15 @@
 "use strict";
 
-// console.log("Buenos dias");
-// setTimeout(() => console.log("¿Como estás?"), 1000 * 2);
-// setTimeout(() => console.log("¿Comó te llamas?"), 1000 * 2);
 const ps = require("prompt-sync");
 const prompt = ps();
 let name = prompt("¿Comó te llamas?   ");
 console.log(`Hola ${name}`);
+
 let mood = prompt("¿Comó te sientes?   ");
 
-if (mood == "bien") {
+if (mood == "bien" || mood == "BIEN" || mood == "Bien") {
   console.log("Genial");
-} else if (mood == "mal") {
+} else if (mood == "mal" || mood == "MAL" || mood == "Mal") {
   console.log("Mucho animo, tu puedes...");
 } else {
   console.log("Lo siento, no te he entendido");
